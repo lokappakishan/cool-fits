d91de44ada92041ead4bda92789243d1f2d05628
KISHAN L <kishan.karthi7@gmail.com>
2022-10-11 21:45:52 +0200

    add styled components

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,10 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+Router.events.on('routerChangeStart', () => NProgress.start());
+Router.events.on('routerChangesComplete', () => NProgress.done());
+Router.events.on('routerChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
