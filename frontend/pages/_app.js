e47228f8e6fa8ba0c2fc3d7f3e320b12f98059dd
KISHAN L <kishan.karthi7@gmail.com>
2022-10-13 09:08:44 +0200

    Visualize route changes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,9 +2,9 @@ import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
 
-Router.events.on('routerChangeStart', () => NProgress.start());
-Router.events.on('routerChangesComplete', () => NProgress.done());
-Router.events.on('routerChangeError', () => NProgress.done());
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
